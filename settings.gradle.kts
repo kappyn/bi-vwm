2ddf622dbe7052b1876e523219e7523083a7e4f7
Filip Kaňák <thebellenik@gmail.com>
2021-03-07 19:20:13 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "Link analysis and web page ranking"
---
+++ src/main/kotlin/cz/cvut/fit/vwm/Application.kt
@@ -0,0 +1,114 @@
+package cz.cvut.fit.vwm
+
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.locations.*
+import io.ktor.features.*
+import io.ktor.gson.*
+import io.ktor.html.*
+import kotlinx.html.*
+import kotlinx.css.*
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+/**
+ * Please note that you can use any other name instead of *module*.
+ * Also note that you can have more then one modules in your application.
+ * */
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(Locations) {
+    }
+
+    install(ContentNegotiation) {
+        gson {
+        }
+    }
+
+
+
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+    }
+    routing {
+        get<MyLocation> {
+            call.respondText("Location: name=${it.name}, arg1=${it.arg1}, arg2=${it.arg2}")
+        }
+        // Register nested routes
+        get<Type.Edit> {
+            call.respondText("Inside $it")
+        }
+        get<Type.List> {
+            call.respondText("Inside $it")
+        }
+    }
+    routing {
+        get("/json/gson") {
+            call.respond(mapOf("hello" to "world"))
+        }
+    }
+    routing {
+        get("/html-dsl") {
+            call.respondHtml {
+                body {
+                    h1 { +"HTML" }
+                    ul {
+                        for (n in 1..10) {
+                            li { +"$n" }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    routing {
+        get("/styles.css") {
+            call.respondCss {
+                body {
+                    backgroundColor = Color.darkBlue
+                    margin(0.px)
+                }
+                rule("h1.page-title") {
+                    color = Color.white
+                }
+            }
+        }
+    }
+    routing {
+        get("/html-css-dsl") {
+            call.respondHtml {
+                head {
+                    link(rel = "stylesheet", href = "/styles.css", type = "text/css")
+                }
+                body {
+                    h1(classes = "page-title") {
+                        +"Hello from Ktor!"
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Location("/location/{name}")
+class MyLocation(val name: String, val arg1: Int = 42, val arg2: String = "default")
+@Location("/type/{name}")
+data class Type(val name: String) {
+    @Location("/edit")
+    data class Edit(val type: Type)
+
+    @Location("/list/{page}")
+    data class List(val type: Type, val page: Int)
+}
+
+suspend inline fun ApplicationCall.respondCss(builder: CSSBuilder.() -> Unit) {
+    this.respondText(CSSBuilder().apply(builder).toString(), ContentType.Text.CSS)
+}
+
