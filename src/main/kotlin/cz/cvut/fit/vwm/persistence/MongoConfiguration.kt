fda58242e4d2ba97c97188957d1646257a4792ce
Kevin Kroupa <kroupkev@fit.cvut.cz>
2021-05-12 21:42:53 +0200

    Mongo config

--- src/main/kotlin/cz/cvut/fit/vwm/persistence/MongoConfiguration.kt
+++ src/main/kotlin/cz/cvut/fit/vwm/persistence/MongoConfiguration.kt
@@ -13,20 +13,27 @@ object MongoConfiguration {
 
     private lateinit var name: String
     private lateinit var password: String
-    private lateinit var srv: String
+    private var srv: String? = null
+    private var host: String? = null
     private lateinit var db : String
     private lateinit var clientSettings: MongoClientSettings
 
     fun setConfig(config: ApplicationConfig) {
         name = config.propertyOrNull("name")?.getString() ?: ""
         password = config.propertyOrNull("password")?.getString() ?: ""
-        srv = config.propertyOrNull("srv")?.getString() ?: ""
+        srv = config.propertyOrNull("srv")?.getString()
+        host = config.propertyOrNull("host")?.getString()
         db = config.propertyOrNull("db")?.getString() ?: ""
 
         clientSettings = MongoClientSettings.builder()
             .credential(MongoCredential.createScramSha1Credential(name, "admin", password.toCharArray()))
             .applyToClusterSettings {
-                it.hosts(mutableListOf(ServerAddress(srv)))
+                if (host != null) {
+                    it.hosts(mutableListOf(ServerAddress(host)))
+                }
+                if (srv != null) {
+                    it.srvHost(srv)
+                }
             }
             .build()
     }
