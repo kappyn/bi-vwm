41f27c88b47815d263aca860b0e483523f459ef9
Filip Kaňák <thebellenik@gmail.com>
2021-05-13 13:52:01 +0200

    Separate environments

--- src/main/kotlin/cz/cvut/fit/vwm/persistence/MongoConfiguration.kt
+++ src/main/kotlin/cz/cvut/fit/vwm/persistence/MongoConfiguration.kt
@@ -19,9 +19,10 @@ object MongoConfiguration {
     private lateinit var password: String
     private var srv: String? = null
     private var host: String? = null
-    private lateinit var db : String
+    private lateinit var db: String
     private lateinit var clientSettings: MongoClientSettings
     private val eventLoopGroup = NioEventLoopGroup()
+    private var env: ENVIRONMENT = ENVIRONMENT.LOCAL
 
     fun setConfig(config: ApplicationConfig) {
         name = config.propertyOrNull("name")?.getString() ?: ""
@@ -29,27 +30,31 @@ object MongoConfiguration {
         srv = config.propertyOrNull("srv")?.getString()
         host = config.propertyOrNull("host")?.getString()
         db = config.propertyOrNull("db")?.getString() ?: ""
+        if (srv != null) {
+            env = ENVIRONMENT.PROD
+        }
 
         clientSettings = MongoClientSettings.builder()
             .credential(MongoCredential.createScramSha1Credential(name, "admin", password.toCharArray()))
-            .retryWrites(true)
-            .writeConcern(WriteConcern.MAJORITY)
-            .applyToConnectionPoolSettings {
-                it.maxConnectionIdleTime(600000, TimeUnit.MILLISECONDS)
-            }
-            .applyToClusterSettings {
-                if (host != null && srv == null) {
-                    it.hosts(mutableListOf(ServerAddress(host)))
+            .environmentTypeSettings()
+            .build()
+    }
+
+    private fun MongoClientSettings.Builder.environmentTypeSettings(): MongoClientSettings.Builder {
+        return when (env) {
+            ENVIRONMENT.LOCAL -> this.applyToClusterSettings { it.hosts(mutableListOf(ServerAddress(host))) }
+            ENVIRONMENT.PROD -> this.streamFactoryFactory(NettyStreamFactoryFactory.builder().eventLoopGroup(eventLoopGroup).build())
+                .applyToSslSettings {
+                    it.enabled(true)
+                }.retryWrites(true)
+                .writeConcern(WriteConcern.MAJORITY)
+                .applyToConnectionPoolSettings {
+                    it.maxConnectionIdleTime(600000, TimeUnit.MILLISECONDS)
                 }
-                if (srv != null) {
+                .applyToClusterSettings {
                     it.srvHost(srv)
                 }
-            }
-            .streamFactoryFactory(NettyStreamFactoryFactory.builder().eventLoopGroup(eventLoopGroup).build())
-            .applyToSslSettings {
-                it.enabled(true)
-            }
-            .build()
+        }
     }
 
     fun newClient(): CoroutineClient {
@@ -58,5 +63,9 @@ object MongoConfiguration {
 
     fun getDatabase(name: String = db): CoroutineDatabase {
         return newClient().getDatabase(name)
+    }
+
+    enum class ENVIRONMENT {
+        LOCAL, PROD
     }
 }
